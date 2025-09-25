5e0e1c6d6484b6026cdf588514b721ad944655ce
24DPTKasa <A240746TK@RVT.lv>
2025-09-25 11:50:43 +0300

    .

---
+++ src/main/java/rvt/Average.java
@@ -0,0 +1,15 @@
+package rvt;
+import java.util.Scanner;
+
+
+public class Average{
+//     public static void main(String[] args){
+//      Scanner scanner = new Scanner(System.in);
+//     int speed = Integer.ValueOf(reader.nextLine);
+//     if (speed > 120) {
+//         System.out.println("Speeding ticket!");
+//     }
+// }
+}
+
+
