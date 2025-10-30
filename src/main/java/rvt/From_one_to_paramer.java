970d8351e71f4e710e6c807726f9b27116e26292
24DPTKasa <A240746TK@RVT.lv>
2025-10-30 11:26:40 +0200

    Jm

---
+++ src/main/java/rvt/From_one_to_paramer.java
@@ -0,0 +1,6 @@
+package rvt;
+public class From_one_to_paramer {
+    public static void printUntilNumber(int number){
+        printUntilNumber(5);
+    }
+}
---
+++ src/main/java/rvt/JmArrayList.java
@@ -0,0 +1,24 @@
+package rvt;
+import java.util.Scanner;
+import java.util.*;
+
+public class JmArrayList {
+
+    public static void main(String[] args) {
+        
+    }
+    public static void OnlyTheseNumbers() {}
+    public static void listSize() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        System.out.println("Ievada vardus:");
+        list.add(scanner.nextInt());
+        System.out.println(list);
+
+        
+
+    }
+    public static void onTheList() {}
+    public static void removeLast(ArrayList<String> strings) {}
+    
+}
