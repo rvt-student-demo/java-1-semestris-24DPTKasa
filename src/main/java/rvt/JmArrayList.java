9e251a23b7a1548b6616192561988fa4203683e7
24DPTKasa <A240746TK@RVT.lv>
2025-11-14 10:43:56 +0200

    izveidoju list size uzd, sakšu pildit onTheList

--- src/main/java/rvt/JmArrayList.java
+++ src/main/java/rvt/JmArrayList.java
@@ -1,4 +1,5 @@
 package rvt;
+
 import java.util.*;
 
 public class JmArrayList {
@@ -6,26 +7,49 @@ public class JmArrayList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
-        while(true){
-        System.out.println("Ievada vardu:");
-    list.add(scanner.nextLine());
-        if( ){
-            break;
+        while (true) {
+            System.out.println("Ievada vardu:");
+            list.add(scanner.nextLine());
+            String inputLine = scanner.nextLine();
+            if (inputLine.isEmpty()) {
+                System.out.println("Search for?");
+                scanner.nextLine();
+                System.out.println(scanner.nextLine());
+                break;
+            }
+
         }
-        
-    }
-        
-        int size = list.size();
-        System.out.println("In total:" + size);
-        
-    }
-    public static void OnlyTheseNumbers() {}
-    public static void listSize() {
-        
-        
+        scanner.close();
 
     }
-    public static void onTheList() {}
-    public static void removeLast(ArrayList<String> strings) {}
-    
+
+    public static void OnlyTheseNumbers() {
+    }
+
+    public static void listSize() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            System.out.println("Ievada vardu:");
+            list.add(scanner.nextLine());
+            String inputLine = scanner.nextLine();
+            if (inputLine.isEmpty()) {
+                break;
+            }
+
+        }
+
+        int size = list.size();
+        System.out.println("In total:" + size);
+        scanner.close();
+
+    }
+
+    public static void onTheList() {
+        
+    }
+
+    public static void removeLast(ArrayList<String> strings) {
+    }
+
 }
