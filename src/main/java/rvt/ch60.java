0b9d1969d9a05dbc92edcdb68fd30f8183c545bd
24DPTKasa <A240746TK@RVT.lv>
2025-11-27 10:22:33 +0200

    uzdevums ch_60

--- src/main/java/rvt/ch60.java
+++ src/main/java/rvt/ch60.java
@@ -2,9 +2,10 @@ package rvt;
 
 public class ch60 {
   public static void main(String[] args) {
+    ex5();
     
   }
-    public static void ex1() {
+  public static void ex1() {
       int[] val = {0, 1, 2, 3}; 
       int sum = 0;
       for (int i = 0; i < val.length; i++)
@@ -12,13 +13,13 @@ public class ch60 {
     System.out.println( "Sum of all numbers = " + sum );
     
   }
-
   public static void ex2() {
 
     int[] val = {13, -4, 82, 17}; 
-    int[] twice = {26, -8, 164, 34};
-    
-    
+    int[] twice = new int[4];
+    for (int i = 0; i < twice.length; i++){
+      twice[i] = val[i] *2;
+    }
     System.out.println( "Original Array: " 
         + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
  
@@ -35,6 +36,71 @@ public class ch60 {
         + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
 
   }
+  public static void ex3() {
+  
+    int[] valA   = { 13, -22,  82,  17}; 
+    int[] valB   = {-12,  24, -79, -13};
+    int[] sum    = {  0,   0,   0,   0};
+
+    sum[0] = valA[0] + valB[0];
+    sum[1] = valA[1] + valB[1];
+    sum[2] = valA[2] + valB[2];
+    sum[3] = valA[3] + valB[3];
 
     
+    // Add values from corresponding cells of valA and valB
+    // and put the result in the corresponding cell of sum.
+
+
+
+ 
+    System.out.println( "sum: " 
+        + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+   
+
+  }
+  public static void ex4() {
+    int[] valA   = { 13, -22,  82,  17}; 
+    int[] valB   = {  12,   47,   -57,   8};
+    for (int i = 0; i < valB.length; i++) {
+      valB[i] = 25 - valA[i];
+    }
+    
+    
+    // Put values into valB so that the sum of the values
+    // in corresponding cells of valA and valB is 25.
+
+
+
+ 
+ 
+    System.out.println( "valA: " 
+        + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
+ 
+    System.out.println( "valB: " 
+        + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
+
+    System.out.println( "sum:  " 
+        + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " 
+        + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );
+
+  }
+  public static void ex5() {
+    int[] val = {0, 1, 2, 3}; 
+    int temp = val[1];
+    
+ 
+  System.out.println( "Original Array: " 
+      + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+ 
+  val[0] = val[3];
+  val[1] = val[2];
+  val[2] = temp;
+  val[3] = temp -1;
+
+    
+ 
+  System.out.println( "Reversed Array: " 
+      + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+  }
 }
