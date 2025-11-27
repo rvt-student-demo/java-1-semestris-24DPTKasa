ac0d4f70c12cd6fa8af2bb18b1b393e0eb0ff5c2
24DPTKasa <A240746TK@RVT.lv>
2025-11-27 09:25:45 +0200

    Chapter 60

--- src/main/java/rvt/ch60.java
+++ src/main/java/rvt/ch60.java
@@ -2,7 +2,7 @@ package rvt;
 
 public class ch60 {
   public static void main(String[] args) {
-    
+    ex2();
   }
     public static void ex1() {
       int[] val = {0, 1, 2, 3}; 
@@ -15,8 +15,9 @@ public class ch60 {
 
   public static void ex2() {
 
-     int[] val = {13, -4, 82, 17}; 
-    int[] twice = val;
+    int[] val = {13, -4, 82, 17}; 
+    int[] twice = {26, -8, 164, 34};
+    
     
     System.out.println( "Original Array: " 
         + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
