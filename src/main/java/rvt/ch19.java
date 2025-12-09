f4259d2ab020fcb1edd96c29db7b805da84bdec1
24DPTKasa <A240746TK@RVT.lv>
2025-12-09 09:54:24 +0200

    nepabegšu ch19 uzdevums

--- src/main/java/rvt/ch19.java
+++ src/main/java/rvt/ch19.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class ch19 {
     public static void main(String[] args) {
-        ex2();
+        ex5();
     }
     public static void ex1() {
         Scanner scanner = new Scanner(System.in);
@@ -34,10 +34,55 @@ public class ch19 {
         
     }
     public static void ex3(){
-            
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter n:");
+        int n = scanner.nextInt();
+        int sum = 0;
+        for (int i = 1; i < n+1; i++){
+            sum += i * i ;
+        }
+        System.out.println("Loop Sum = " + sum);
+
+        int sum1 = n;
+        sum1 = (n*(n+1)*(2*n+1))/6;
+        System.out.println("formula Sum = " + sum1);
+        scanner.close();
+
+
+
+
     }
-    public static void ex4(){}
-    public static void ex5(){}
+    public static void ex4(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter low: ");
+        int low = scanner.nextInt();
+        System.out.println("Enter high: ");
+        int n = scanner.nextInt();
+        int sumhigh = 0;
+        int sumlow = 0;
+        for (int i = 1; i < n +1; i ++){
+            sumhigh += i;
+        }
+        for (int i = 1; i <low ; i ++){
+            sumlow += i;
+        }
+        System.out.println("Sumhigh = " + sumhigh);
+        System.out.println("Sumlow = " + sumlow);
+        
+        int sum = sumhigh - sumlow;
+        System.out.println("sum = " + sum);
+       
+
+    }
+    public static void ex5(){
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        System.out.println(word);
+        int times;
+        times = word.length();
+        System.out.println(times);
+        
+    }
     public static void ex6(){}
     public static void ex7(){}
 
