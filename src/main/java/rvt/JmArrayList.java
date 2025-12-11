3db757f022327517e51ce40045c491b49187f311
24DPTKasa <A240746TK@RVT.lv>
2025-12-11 10:15:54 +0200

    JmArrayList

--- src/main/java/rvt/JmArrayList.java
+++ src/main/java/rvt/JmArrayList.java
@@ -5,27 +5,33 @@ import java.util.*;
 public class JmArrayList {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
-        while (true) {
-            System.out.println("Ievada vardu:");
-            list.add(scanner.nextLine());
-            String inputLine = scanner.nextLine();
-            if (inputLine.isEmpty()) {
-                System.out.println("Search for?");
-                if (list.contains(scanner.nextLine())) {
-                    System.out.println(scanner.nextLine() + "was found");
-                }
-                break;
-            }
-        }
-        scanner.close();
-
+      OnlyTheseNumbers();
     }
 
     public static void OnlyTheseNumbers() {
-    }
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
 
+        while (true) {
+            System.out.println("Ievada ciparu (Enter -1 to end): ");
+            int num = scanner.nextInt();
+            if (num == -1) {
+                break;
+            }
+            numbers.add(num);
+        }
+
+        System.out.println("From where? ");
+        int start = scanner.nextInt();
+
+        System.out.println("To where? ");
+        int end = scanner.nextInt();
+        System.out.println(" ");
+        for (int i = start; i <= end; i++) {
+            System.out.println(numbers.get(i));
+        }
+        scanner.close();
+    }
     public static void listSize() {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
@@ -44,12 +50,50 @@ public class JmArrayList {
         scanner.close();
 
     }
-
     public static void onTheList() {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> names = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Ievada vardu (press Enter to start searching):");
+            String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+            names.add(input);
+        }
+
+        System.out.print("Search for? ");
+        String search = scanner.nextLine();
+
+        if (names.contains(search)) {
+            System.out.println(search + " was found!");
+        } else {
+            System.out.println(search + " was not found!");
+        }
+        scanner.close();
         
     }
-
     public static void removeLast(ArrayList<String> strings) {
+        if (strings.size() > 0) {
+            strings.remove(strings.size() - 1);
+        }
+    }
+    public static void removeLastUzd() {
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+
+        System.out.println(strings);
+
+        removeLast(strings);
+        removeLast(strings);
+
+        System.out.println(strings);
+        
     }
 
 }
